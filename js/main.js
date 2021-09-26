0306f3a00ffd456913840729133735c4684c955c
Aleksander <sandrix@ukr.net>
2021-09-26 13:42:50 +0300

    products section done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     $('.banner-section__slider').slick({
         dots: true,
-        prevArrow: '<button class="banner-section__inner-btn banner-section__inner-btnprev"><img src="/images/left-arrow.svg"     alt=""></button> ',
-        nextArrow: `<button class="banner-section__inner-btn banner-section__inner-btnnext"><img src="/images/right-arrow.svg" alt="" ></button> `
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="/images/left-arrow.svg"     alt=""></button> ',
+        nextArrow: `<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="/images/right-arrow.svg" alt="" ></button> `
     });
 
     // перемикач табів
@@ -21,10 +21,12 @@ $(function () {
         $(this).toggleClass('favorite-active')
     });
 
-    // слайдер популярних товарів
-    // $('.products-slider__item').slick({
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1
-    // });
+    // слайдер популярних товарів - не працює
+    $('.products-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="products-slider__slider-btn products-slider__slider-btnprev"><img src="/images/left-arrow.svg"     alt=""></button> ',
+        nextArrow: `<button class="products-slider__slider-btn products-slider__slider-btnnext"><img src="/images/right-arrow.svg" alt="" ></button> `
+    });
 
 });
